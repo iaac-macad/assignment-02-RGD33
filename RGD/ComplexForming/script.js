3ea1d0b9cc51c0f8e2e258ab1d3e5917e9bf6b94
RGD33 <raghad.hammour@students.iaac.net>
2023-02-01 00:10:05 +0200

    Added attributes

--- RGD/ComplexForming/script.js
+++ RGD/ComplexForming/script.js
@@ -3,18 +3,15 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { Rhino3dmLoader } from 'three/addons/loaders/3DMLoader.js'
 
-// declare variables to store scene, camera, and renderer
-let scene, camera, renderer
-const model = 'RAGHADHAMMOUR.3dm'
+let camera, scene, raycaster, renderer
+const mouse = new THREE.Vector2()
+window.addEventListener('click', onClick);
 
-// call functions
 init()
 animate()
 
-// function to setup the scene, camera, renderer, and load 3d model
-function init () {
+function init() {
 
-    // Rhino models are z-up, so set this as the default
     THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 )
 
     // create a scene and a camera
@@ -30,33 +27,99 @@ function init () {
     renderer.setSize( window.innerWidth, window.innerHeight )
     document.body.appendChild( renderer.domElement )
 
-    // add some controls to orbit the camera
     const controls = new OrbitControls( camera, renderer.domElement )
 
-    // add a directional light
     const directionalLight = new THREE.DirectionalLight( 0xffffff )
+    directionalLight.position.set( 0, 0, 2 )
+    directionalLight.castShadow = true
     directionalLight.intensity = 2
     scene.add( directionalLight )
 
-    // load the model
+    raycaster = new THREE.Raycaster()
+
     const loader = new Rhino3dmLoader()
     loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@7.11.1/' )
 
-    loader.load( model, function ( object ) {
+    loader.load( 'RAGHADHAMMOUR.3dm', function ( object ) {
 
+        document.getElementById('loader').remove()
         scene.add( object )
+        console.log( object )
 
     } )
 
 }
 
-// function to continuously render the scene
+function onClick( event ) {
+
+    console.log( `click! (${event.clientX}, ${event.clientY})`)
+
+	// calculate mouse position in normalized device coordinates
+    // (-1 to +1) for both components
+
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1
+    
+    raycaster.setFromCamera( mouse, camera )
+
+	// calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObjects( scene.children, true )
+
+    let container = document.getElementById( 'container' )
+    if (container) container.remove()
+
+    // reset object colours
+    scene.traverse((child, i) => {
+        if (child.isMesh) {
+            child.material.color.set( 'white' )
+        }
+    });
+
+    if (intersects.length > 0) {
+
+        // get closest object
+        const object = intersects[0].object
+        console.log(object) // debug
+
+        object.material.color.set( 'yellow' )
+
+        // get user strings
+        let data, count
+        if (object.userData.attributes !== undefined) {
+            data = object.userData.attributes.userStrings
+        } else {
+            // breps store user strings differently...
+            data = object.parent.userData.attributes.userStrings
+        }
+
+        // do nothing if no user strings
+        if ( data === undefined ) return
+
+        console.log( data )
+        
+        // create container div with table inside
+        container = document.createElement( 'div' )
+        container.id = 'container'
+        
+        const table = document.createElement( 'table' )
+        container.appendChild( table )
+
+        for ( let i = 0; i < data.length; i ++ ) {
+
+            const row = document.createElement( 'tr' )
+            row.innerHTML = `<td>${data[ i ][ 0 ]}</td><td>${data[ i ][ 1 ]}</td>`
+            table.appendChild( row )
+        }
+
+        document.body.appendChild( container )
+    }
+
+}
+
 function animate() {
 
     requestAnimationFrame( animate )
     renderer.render( scene, camera )
 
 }
-
-
 
